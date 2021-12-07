150bd36ed3b3dff6a9f6d1aac84b6642bb192565
Aclaez <aclaez@gmail.com>
2021-12-07 17:43:14 +0300

    Delete build.bat
    delete build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-"C:\Program Files\Epic Games\UE_4.26\Engine\Build\BatchFiles\RunUAT.bat" ^
-BuildCookRun ^
--project="C:\Users\trs_user\Documents\Unreal Projects\MyGame\ShootThemUp.uproject" ^
--platform=Win64 -clientconfig=Shipping ^
--archivedirectory="C:\Users\trs_user\Documents\Unreal Projects\MyGame\BuildFromBat" ^
--build -cook -stage ^
--stagingdirectory="C:\Users\trs_user\Documents\Unreal Projects\MyGame\Stage" ^
--archive -pak -package 
